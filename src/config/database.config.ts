ba6eb3e4a1f84c547e74c2a9084a63506329b7b9
Will Veic <will@willveic.com>
2024-12-15 12:32:24 +0100

    minor string change

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from "@nestjs/config";
 
 export default registerAs('database', () => ({
-    type: "postgres",
+    type: 'postgres',
     host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT, 10) || 5432,
     username: process.env.DB_USERNAME,
