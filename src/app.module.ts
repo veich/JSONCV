a9f4226406b02dcd9a32d9a61032d7fb852ca30e
Will Veic <will@willveic.com>
2024-12-15 20:04:24 +0100

    cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
