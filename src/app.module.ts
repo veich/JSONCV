0984338a28505d1c4e2802d9e6d481deae34d277
Will Veic <will@willveic.com>
2024-12-15 20:03:34 +0100

    endpoint cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -22,47 +21,7 @@ const configService = new ConfigService();
   }),
     AuthModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule { }
-
-
-
-
-
-
-
-
-// import { Module } from '@nestjs/common';
-// import { Teacher } from './teacher.entity';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-// import { AuthModule } from './auth/auth.module';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-// import { ConfigModule, ConfigService } from '@nestjs/config';
-
-// const configService = new ConfigService();
-
-// @Module({
-//   imports: [ConfigModule.forRoot({
-//     isGlobal: true,
-//     envFilePath: '.env',
-//     cache: true,
-//   }), TypeOrmModule.forRoot({
-//     type: 'postgres',
-//     host: configService.getOrThrow('DB_HOST'),
-//     port: Number(configService.getOrThrow('DB_PORT')),
-//     username: configService.getOrThrow('DB_USERNAME'),
-//     password: configService.getOrThrow('DB_PASSWORD'),
-//     database: configService.getOrThrow('DB_DATABASE'),
-//     entities: [Teacher],
-//     synchronize: false, // true for dev only
-//     // migrations: ['dist/src/db/migrations/**'],
-//   }),
-//     AuthModule
-//   ],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
-// export class AppModule { }
