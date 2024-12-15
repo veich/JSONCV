2c1b034e5fe7e8b4d207eec3d756ab1cd118ff89
Will Veic <will@willveic.com>
2024-12-15 11:56:26 +0100

    removed unneccessary import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { Teacher } from './teacher.entity';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
