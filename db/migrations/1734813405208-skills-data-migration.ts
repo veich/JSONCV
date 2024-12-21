7da9a39378f04dd5362651b463c4463541010733
Will Veic <will@willveic.com>
2024-12-21 22:00:41 +0100

    skills - prefilled DB table with skills data

---
+++ db/migrations/1734813405208-skills-data-migration.ts
@@ -0,0 +1,24 @@
+import { Skill } from "src/skills/entities/skill.entity";
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class SkillsDataMigration1734813405208 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        const skillsRepositiory = queryRunner.manager.getRepository(Skill);
+        [
+            'NodeJS', 'TypeScript', 'Sequelize', 'TypeORM', 'PostgreSQL',
+            'Objective-C', 'Java', 'Swift', 'Kotlin', 'Python'
+        ].forEach(async (skillName) => {
+            const skill = await skillsRepositiory.create({ skillName });
+            await skillsRepositiory.save(skill);
+        });
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.manager
+            .createQueryBuilder()
+            .delete()
+            .from(Skill)
+            .execute()
+    }
+}
