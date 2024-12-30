cb259475c3111ccf05fcb8084d5cbea22e9d41e8
Will Veic <will@willveic.com>
2024-12-30 15:45:38 +0100

    fix - added missing config provider

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,12 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, ConfigService],
   exports: [UsersService],
 })
 export class UsersModule { }
