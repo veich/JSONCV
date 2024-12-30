e8f5f312a5e5a1362ec515a1833d67250cb8c152
Will Veic <will@willveic.com>
2024-12-30 17:46:52 +0100

    tests - implemented returning users and user by id

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -8,8 +8,46 @@ import { User } from '../src/users/entities/user.entity';
 describe('UsersController (e2e)', () => {
     let app: INestApplication;
 
+    const mockedUsers = [
+        {
+            id: 1,
+            name: 'John',
+            email: 'john@email.com',
+            experiences: [{
+                id: 1001,
+                position: { id: 2001, positionName: 'FE Developer' },
+                skills: [
+                    { id: 3001, skillName: 'JavaScript' },
+                    { id: 3002, skillName: 'HTML' },
+                    { id: 3003, skillName: 'CSS' },
+                ]
+            }],
+        },
+        {
+            id: 2,
+            name: 'Mary',
+            email: 'mary@email.com',
+            experiences: [{
+                id: 1002,
+                position: { id: 2002, positionName: 'iOS Developer' },
+                skills: [
+                    { id: 3004, skillName: 'Objective-C' },
+                    { id: 3005, skillName: 'Swift' },
+                    { id: 3006, skillName: 'CSS' },
+                ]
+            }],
+        },
+    ];
+
     const mockUsersRepo = {
-        find: jest.fn(() => []),
+        find: jest.fn().mockResolvedValue(mockedUsers),
+        findOne: jest.fn().mockImplementation((query) =>
+            Promise.resolve(mockedUsers.find((user) => user.id === query.where.id))
+        ),
+        // create: jest.fn().mockImplementation((dto) => dto),
+        // save: jest.fn().mockImplementation((user) =>
+        //     Promise.resolve({ id: Date.now(), ...user })
+        // ),
     };
 
     beforeEach(async () => {
@@ -28,6 +66,22 @@ describe('UsersController (e2e)', () => {
         return request(app.getHttpServer())
             .get('/users')
             .expect(200)
-            .expect([]);
+            // .expect(mockedUsers);
+            .then((response) => {
+                expect(response.body).toEqual(mockedUsers);
+                expect(response.body[0].skills.sort()).toEqual(['JavaScript', 'HTML', 'CSS'].sort());
+                expect(response.body[1].skills.sort()).toEqual(['Objective-C', 'Swift', 'CSS'].sort());
+            });
+    });
+
+    it('/users/2 (GET)', () => {
+        return request(app.getHttpServer())
+            .get('/users/2')
+            .expect(200)
+            // .expect(mockedUsers);
+            .then((response) => {
+                expect(response.body).toEqual(mockedUsers[1]);
+                expect(response.body.skills.sort()).toEqual(['Objective-C', 'Swift', 'CSS'].sort());
+            });
     });
 });
